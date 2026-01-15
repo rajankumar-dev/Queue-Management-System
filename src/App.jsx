661c8c809c81161f3c893288dc19ed36a0dc57b0
rajankumar-dev <rk1620703@gmail.com>
2026-01-15 22:56:10 +0530

    feat: implement initial styling and layout for the Queue Management Application

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import "./App.css";
+import QueueForm from "./components/QueueForm.jsx";
 
 function App() {
+  const [queue, setQueue] = useState([]);
+
+  const addToQueue = (customer) => {};
+
+  const removeFromQueue = (id) => {};
+
+  const updateStatus = (Id, newStatus) => {};
+
   return (
-    <>
-      <h1>QMS</h1>
-    </>
+    <div className="app">
+      <header>
+        <h1>Queue Management Application</h1>
+        <p>Manage your customers efficeintly</p>
+      </header>
+      <main>
+        <QueueForm onAdd={addToQueue} />
+        <h1>QueueDisplay</h1>
+      </main>
+    </div>
   );
 }
 
